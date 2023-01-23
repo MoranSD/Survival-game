5881ac5beed9b2f7eef7a650cd933377d4a0f859
MoranSD <93720897+MoranSD@users.noreply.github.com>
2023-01-23 07:40:17 +0300

    better inventory

--- Assets/Scripts/InventorySystem/Inventory.cs
+++ Assets/Scripts/InventorySystem/Inventory.cs
@@ -6,29 +6,27 @@ namespace InventorySystem
 {
     public class Inventory : MonoBehaviour
     {
-        /*
-         * тут помимо основных данных предмета, можно будет добавить уникальную для каждого
-         */
+        public static Inventory Instance;
+
         public Dictionary<Vector2Int, GameItemInventoryData> Items { get; private set; }
 
         public const int Columns = 5;
         public const int Rows = 5;
         public const int FastSlotsCount = 5;
 
-        [SerializeField] private Transform _mainCellsContainer;
-        [SerializeField] private Transform _fastCellsContainer;
-        [SerializeField] private InventoryCell _cellPrefab;
-
-        private InventoryCell[] _mainCells;
-        private InventoryCell[] _fastCells;
+        [SerializeField] private UIInventory _uiInventory;
 
         private void Awake()
         {
-            Items = new Dictionary<Vector2Int, GameItemInventoryData>();
-            CreateCells();
+            if (Instance == null) Instance = this;
+            else Destroy(this.gameObject);
 
+            Items = new Dictionary<Vector2Int, GameItemInventoryData>();
+        }
+        private void Start()
+        {
             if (TryLoadData())
-                UpdateCells();
+                _uiInventory.UpdateCells();
         }
         private void Update()
         {
@@ -54,113 +52,19 @@ namespace InventorySystem
         public bool TryAddItem(GameItem gameItem)
         {
             InventoryCell freeCell;
-            if (gameItem.BaseData.IsStackable) freeCell = GetFreeCellWithStackableItem(gameItem.BaseData.Id, gameItem.BaseData.currentCount);
-            else freeCell = GetFreeCell();
+            if (gameItem.BaseData.IsStackable) freeCell = _uiInventory.GetFreeCellWithStackableItem(gameItem.BaseData.Id, gameItem.BaseData.currentCount);
+            else freeCell = _uiInventory.GetFreeCell();
 
             if (freeCell != null)
             {
                 if (freeCell.IsEmpty) Items.Add(freeCell.GridPosition, new GameItemInventoryData(gameItem.BaseData, gameItem.UnicData));
                 else Items[freeCell.GridPosition].baseData.currentCount += gameItem.BaseData.currentCount;
 
-                freeCell.Render(Items[freeCell.GridPosition].baseData);
+                _uiInventory.UpdateCell(freeCell);
                 return true;
             }
 
             return false;
-        }
-        private InventoryCell GetFreeCell()
-        {
-            for (int i = 0; i < _mainCells.Length; i++)
-                if (_mainCells[i].IsEmpty) return _mainCells[i];
-
-            for (int i = 0; i < _fastCells.Length; i++)
-                if (_fastCells[i].IsEmpty) return _fastCells[i];
-
-            return null;
-        }
-        private InventoryCell GetFreeCellWithStackableItem(int itemId, int neededSlotsCount)
-        {
-            if (CheckCells(_mainCells, out InventoryCell mainCell)) return mainCell;
-            if (CheckCells(_fastCells, out InventoryCell fastCell)) return fastCell;
-            return GetFreeCell();
-
-
-
-            bool CheckCells(InventoryCell[] cells, out InventoryCell returnCell)
-            {
-                for (int i = 0; i < cells.Length; i++)
-                {
-                    if (cells[i].IsEmpty == false)
-                    {
-                        BaseGameItemData itemData = Items[cells[i].GridPosition].baseData;
-                        if (itemData.Id == itemId)
-                        {
-                            int freeSlotsInCell = itemData.maxStackCount - itemData.currentCount;
-                            if (freeSlotsInCell >= neededSlotsCount)
-                            {
-                                returnCell = cells[i];
-                                return true;
-                            }
-                        }
-                    }
-                }
-
-                returnCell = null;
-                return false;
-            }
-        }
-        private void UpdateCells()
-        {
-            for (int i = 0; i < _mainCells.Length; i++)
-            {
-                if (Items.ContainsKey(_mainCells[i].GridPosition))
-                {
-                    BaseGameItemData nextItemData = Items[_mainCells[i].GridPosition].baseData;
-                    _mainCells[i].Render(nextItemData);
-                }
-                else
-                {
-                    _mainCells[i].ResetRender();
-                }
-            }
-            for (int i = 0; i < _fastCells.Length; i++)
-            {
-                if (Items.ContainsKey(_fastCells[i].GridPosition))
-                {
-                    BaseGameItemData nextItemData = Items[_fastCells[i].GridPosition].baseData;
-                    _fastCells[i].Render(nextItemData);
-                }
-                else
-                {
-                    _fastCells[i].ResetRender();
-                }
-            }
-        }
-
-        private void CreateCells()
-        {
-            _mainCells = new InventoryCell[Columns * Rows];
-
-            int i = 0;
-            for (int y = Columns - 1; y >= 0; y--)
-            {
-                for (int x = 0; x < Rows; x++)
-                {
-                    InventoryCell cell = Instantiate(_cellPrefab, _mainCellsContainer);
-                    cell.GridPosition = new Vector2Int(x, y);
-                    _mainCells[i] = cell;
-                    i++;
-                }
-            }
-
-            _fastCells = new InventoryCell[FastSlotsCount];
-
-            for (int x = 0; x < FastSlotsCount; x++)
-            {
-                InventoryCell cell = Instantiate(_cellPrefab, _fastCellsContainer);
-                cell.GridPosition = new Vector2Int(x, -1);//-1 bcz fast slots below main slots
-                _fastCells[x] = cell;
-            }
         }
 
         private bool TryLoadData()
---
+++ Assets/Scripts/InventorySystem/UIInventory.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using GameItems;
+using System.Collections.Generic;
+
+namespace InventorySystem
+{
+	public class UIInventory : MonoBehaviour
+	{
+        [SerializeField] private Transform _mainCellsContainer;
+        [SerializeField] private Transform _fastCellsContainer;
+        [SerializeField] private InventoryCell _cellPrefab;
+
+        private InventoryCell[] _mainCells;
+        private InventoryCell[] _fastCells;
+
+        private void Awake()
+        {
+            CreateCells();
+        }
+
+        private void CreateCells()
+        {
+            _mainCells = new InventoryCell[Inventory.Columns * Inventory.Rows];
+
+            int i = 0;
+            for (int y = Inventory.Columns - 1; y >= 0; y--)
+            {
+                for (int x = 0; x < Inventory.Rows; x++)
+                {
+                    InventoryCell cell = Instantiate(_cellPrefab, _mainCellsContainer);
+                    cell.GridPosition = new Vector2Int(x, y);
+                    _mainCells[i] = cell;
+                    i++;
+                }
+            }
+
+            _fastCells = new InventoryCell[Inventory.FastSlotsCount];
+
+            for (int x = 0; x < Inventory.FastSlotsCount; x++)
+            {
+                InventoryCell cell = Instantiate(_cellPrefab, _fastCellsContainer);
+                cell.GridPosition = new Vector2Int(x, -1);//-1 bcz fast slots below main slots
+                _fastCells[x] = cell;
+            }
+        }
+        public void UpdateCells()
+        {
+            Dictionary<Vector2Int, GameItemInventoryData> Items = Inventory.Instance.Items;
+
+            for (int i = 0; i < _mainCells.Length; i++)
+            {
+                if (Items.ContainsKey(_mainCells[i].GridPosition))
+                {
+                    BaseGameItemData nextItemData = Items[_mainCells[i].GridPosition].baseData;
+                    _mainCells[i].Render(nextItemData);
+                }
+                else
+                {
+                    _mainCells[i].ResetRender();
+                }
+            }
+            for (int i = 0; i < _fastCells.Length; i++)
+            {
+                if (Items.ContainsKey(_fastCells[i].GridPosition))
+                {
+                    BaseGameItemData nextItemData = Items[_fastCells[i].GridPosition].baseData;
+                    _fastCells[i].Render(nextItemData);
+                }
+                else
+                {
+                    _fastCells[i].ResetRender();
+                }
+            }
+        }
+        public void UpdateCell(InventoryCell cell)
+        {
+            if (Inventory.Instance.Items.ContainsKey(cell.GridPosition))
+            {
+                BaseGameItemData itemData = Inventory.Instance.Items[cell.GridPosition].baseData; 
+                cell.Render(itemData);
+            }
+            else
+            {
+                throw new System.Exception("There is no item data for this cell");
+            }
+        }
+        public InventoryCell GetFreeCell()
+        {
+            for (int i = 0; i < _mainCells.Length; i++)
+                if (_mainCells[i].IsEmpty) return _mainCells[i];
+
+            for (int i = 0; i < _fastCells.Length; i++)
+                if (_fastCells[i].IsEmpty) return _fastCells[i];
+
+            return null;
+        }
+        public InventoryCell GetFreeCellWithStackableItem(int itemId, int neededSlotsCount)
+        {
+            if (CheckCells(_mainCells, out InventoryCell mainCell)) return mainCell;
+            if (CheckCells(_fastCells, out InventoryCell fastCell)) return fastCell;
+            return GetFreeCell();
+
+
+
+            bool CheckCells(InventoryCell[] cells, out InventoryCell returnCell)
+            {
+                for (int i = 0; i < cells.Length; i++)
+                {
+                    if (cells[i].IsEmpty == false)
+                    {
+                        BaseGameItemData itemData = Inventory.Instance.Items[cells[i].GridPosition].baseData;
+                        if (itemData.Id == itemId)
+                        {
+                            int freeSlotsInCell = itemData.maxStackCount - itemData.currentCount;
+                            if (freeSlotsInCell >= neededSlotsCount)
+                            {
+                                returnCell = cells[i];
+                                return true;
+                            }
+                        }
+                    }
+                }
+
+                returnCell = null;
+                return false;
+            }
+        }
+    }
+}
